876ece045acb2ea315ba7ad1bdab2b1fffdb8b63
Garrett <gflynn@usc.edu>
2020-11-28 15:28:20 -0600

    Pushed new JS logic and disclaimer

--- docs/components/Disclaimer.js
+++ docs/components/Disclaimer.js
@@ -6,17 +6,17 @@ class Disclaimer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div class="disclaimer-page">
-        <h2 id="disclaimer-title">Disclaimer:</h2>
-        <h3 id="disclaimer-description">
-          We gather your data and it will be publicly visible (though anonymous)
-        </h3>
-
-        <p id="disclaimer-description">[D1]. Show an example display of data.</p><br />
-        
         <div class="form">
-          <h3>Please provide your email</h3>
-          <h4>This is to send a user ID number that you can use to remove data.</h4>
-          <input class="form-input" type="text" name="email" required />
+          <h2 id="disclaimer-title">Welcome to On Memory Neurotechnologies</h2>
+          <h3>
+            In this experience, we will publicly display your <strong>anonymous</strong> feedback 
+            (submitted on embedded forms) to gather public preferences 
+            on cognitive neurostimulation devices.
+          </h3>
+        
+          <h3>Here is your User ID</h3>
+          <h4>Save this so that, if requested, we can remove your data.</h4>
+          <p id="user-id"></p>
           <br /><br />
           
           <h3>How old are you?</h3>
@@ -26,7 +26,7 @@ class Disclaimer extends HTMLElement {
 
           <input type="checkbox" id="consent" name="consent" required />
           <label for="consent">
-            I consent to the use of my data in the <a href="#">linked document</a>
+            I am at least 18 years of age and consent to the collection and display of my data for On Memory Neurotechnologies
           </label><br />
         </div>
       </div>
---
+++ docs/js/data.js
@@ -0,0 +1,10 @@
+function checkConsent(){
+    let consentBox = document.getElementById("consent");
+    collectData = consentBox.checked;
+}
+
+function submitForm(){
+
+}
+
+
