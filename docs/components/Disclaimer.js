f9a1a69bbc41a5c7ce45fd3d8a2937fd95cb00e8
Garrett <gflynn@usc.edu>
2020-11-28 16:55:01 -0600

    Enabled consent to collect data

--- docs/components/Disclaimer.js
+++ docs/components/Disclaimer.js
@@ -6,29 +6,28 @@ class Disclaimer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div class="disclaimer-page">
-        <div class="form">
-          <h2 id="disclaimer-title">Welcome to On Memory Neurotechnologies</h2>
-          <h3>
-            In this experience, we will publicly display your <strong>anonymous</strong> feedback 
-            (submitted on embedded forms) to gather public preferences 
-            on cognitive neurostimulation devices.
-          </h3>
-          <br /><br />
+        <div id="disclaimer-form" class="form">
+          <div class="center-text">
           <h3>Here is your User ID:</h3>
-          <h4>Save this so that, if requested, we can remove your data.</h4>
+          <h4>Please save this so that, if requested, we can remove your data.</h4>
           <p id="user-id"></p>
+          </div>
           <br /><br />
-          
-          <h3>How old are you?</h3>
-          <h4>You must be 18 or older to participate.</h4>
-          <input class="form-input" type="text" name="age" required />
-          <br /><br />
-
+          <h3>
+            In this experience, we will collect and publicly display your <strong>anonymous</strong> responses 
+            to understand public preferences about cognitive neurostimulation devices.
+          </h3>
+          <br/>
           <input type="checkbox" id="consent" name="consent" required />
           <label for="consent">
             I am at least 18 years of age and consent to the collection and display of my data for On Memory Neurotechnologies
-          </label><br />
+          </label>
+<!--          <h3>How old are you?</h3>-->
+<!--          <h4>You must be 18 or older to participate.</h4>-->
+<!--          <input class="form-input" type="text" name="age" required />-->
+<!--          <br /><br />-->
         </div>
+        <div id="warning-message"></div>
       </div>
     `;
   }
--- docs/js/data.js
+++ docs/js/data.js
@@ -6,7 +6,6 @@ function displayUserId(){
     }
 }
 
-
 function clientAction(destination,method){
     fetch(url + destination, { method: method,
         mode: 'cors',
@@ -36,6 +35,37 @@ function showMessage(res) {
 function checkConsent(){
     let consentBox = document.getElementById("consent");
     collectData = consentBox.checked;
+    if (collectData) {
+        submitForm();
+        changeDeck(1)
+    } else {
+        if (numTries == 0) {
+            let buttons = document.getElementById('disclaimer').getElementsByTagName("button")
+            for (let button of buttons) {
+                button.style.display = 'none';
+            }
+            document.getElementById('disclaimer-form').style.display = 'none';
+            document.getElementById('warning-message').innerHTML = `
+<h3>Are you sure you want to move forward without data collection?</h3>
+<div id="disclaimer-buttons">
+    <a onclick="changeDeck(1); numTries = 0;" id="button1" class="black">Yes</a>
+    <a onclick="unhideForm()" id="button2" class="black">No</a>
+</div>
+`;
+            numTries++;
+        }
+    }
+}
+
+function unhideForm(){
+    let buttons = document.getElementById('disclaimer').getElementsByTagName("button")
+    for (let button of buttons) {
+        button.style.display = 'none';
+    }
+    document.getElementById('disclaimer-form').style.display = 'block';
+    document.getElementById('warning-message').innerHTML = '';
+    insertProgressButton();
+    numTries = 0;
 }
 
 function submitForm(){
