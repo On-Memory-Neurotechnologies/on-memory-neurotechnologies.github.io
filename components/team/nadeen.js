d02844e6cd0cdaedad5db776172334fa059284af
Garrett <gflynn@usc.edu>
2020-11-30 13:08:42 -0600

    updated public site

--- components/team/nadeen.js
+++ components/team/nadeen.js
@@ -8,7 +8,7 @@ class NadeenMohamed extends HTMLElement {
                     <h3>Nadeen Mohamed</h3>
                     <h4>Full-Stack Developer</h4>
                     <br/>
-                    <p>During NeuroNexus 2020, Nadeen was a ________</p>
+                    <p>Nadeen is a fourth-year BSc student at the University of Alberta, currently pursuing a degree in Software Practice but with a background in Neuroscience. She is a back-end developer for On Memory Neurotechnologies and works on the implementation of the interactive article. When not hard at work with school and other programming extracurriculars, she enjoys woodworking, reading, baking, and playing virtual board games with friends.</p>
                     <br/>
 `;
     }
