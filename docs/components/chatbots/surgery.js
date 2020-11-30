faf44b9da4fa8d46bffb40f2febbab25ebd29373
Garrett <gflynn@usc.edu>
2020-11-30 00:33:09 -0600

    Finished all chatbots and custom components. Only styling required

---
+++ docs/components/chatbots/surgery.js
@@ -0,0 +1,26 @@
+class surgeryChatBot extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+<div class="grid dark">
+        <div class="paragraph-left">
+            <p>Time for Pat's surgery!</p>
+            <h4>Click on Dr. Singh</h4>
+        </div>
+        <div class="paragraph-right">
+        <div class="flex">
+        <a onclick="fullscreenChat('surgery'); runChat(surgeryChat);"><img id="pat-image" src="assets/singh.svg"/></a>
+        </div>
+        <div class="chatbot" style='display: none'">
+                 <ul id="surgery-messages" class="messages"></ul>
+        </div>
+        </div>
+    </div>
+    `;
+    }
+}
+
+customElements.define("surgery-chatbot", surgeryChatBot);
