71f95b53a21c964978332592baa0de11b4679e20
Garrett <gflynn@usc.edu>
2020-12-01 09:02:39 -0600

    updated public site

--- components/team/eugene.js
+++ components/team/eugene.js
@@ -8,7 +8,7 @@ class EugeneLok extends HTMLElement {
                     <h3>Eugene Lok</h3>
                     <h4>Back-End Developer</h4>
                     <br/>
-                    <p><i>Coming soon...</i></p>
+                    <p>Eugene is a fourth-year Geomatics Engineering student at the University of Calgary. For the Neuro Nexus 2020 challenge, he utilizes his experience with developing web applications with JavaScript to assist with the On Memory Neurotechnologies team. During his spare time, Eugene likes to bike, hang out with friends, and find new places to eat.</p>
                     <br/>                    
 `;
     }
