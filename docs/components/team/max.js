0021f0611f8b752c90eb2ee5027ba3803cd43ae5
Garrett <gflynn@usc.edu>
2020-12-01 09:02:22 -0600

    Added Max and Eugene descriptions

--- docs/components/team/max.js
+++ docs/components/team/max.js
@@ -8,7 +8,7 @@ class MaxEisele extends HTMLElement {
                     <h3>Max Eisele</h3>
                     <h4>Pitch Organizer</h4>
                     <br/>
-                    <p><i>Coming soon...</i></p>
+                    <p>Max is a Master of Science student in Kinesiology at the University of Calgary. His research focuses on cancer survivors and how to support them to live active lives. Max also runs a volunteer based spinal cord and stroke rehabilitation afterhours program at the Foothills hospital. As a mixed methods researcher Max’s role during the NeuroNexus competition was the focus on knowledge translation and creating presentations on the progress of the article.</p>
                     <br/>
 `;
     }
